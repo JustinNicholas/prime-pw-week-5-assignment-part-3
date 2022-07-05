f89d17e30b03e24dbbb6b1b11f04e92571e2165a
Justin Nicholas <nicholasj964@yahoo.com>
2022-07-05 11:21:42 -0500

    completed the showCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -14,8 +14,20 @@ function addToCollection( songName, artistName, year) {
 
 addToCollection( 'Falling Down', 'Lil Peep', 2018);
 addToCollection( 'Kickstart My Heart', 'Motley Crue', 1989);
-addToCollection( 'Sleeping on the Blacktop', 'Colter Wall', 2015);
+addToCollection( 'Star Shopping', 'Lil Peep', 2017);
 addToCollection( 'Title Track', 'MGK', 2020);
 addToCollection( 'Rapid Roy', 'Jim Croce', 1972);
 addToCollection( 'Mama Tried', 'Merle Haggard & The Strangers', 1968);
 
+console.log(collection);
+
+function showCollection( collectionIndex ) {
+    console.log('The number ' + collectionIndex + " song in this collection is " + collection[collectionIndex].title);
+    console.log('There are ' + collection.length + ' songs in this collection');
+
+    for ( let i = 0; i<collection.length; i++) {
+        console.log( collection[i].title + ' by ' + collection[i].artist + ' published in ' + collection[i].yearPublished);
+    }
+}
+
+showCollection(1);
