ffd2d89e45c11b881f60118167adbf9e539e06e6
Justin Nicholas <nicholasj964@yahoo.com>
2022-07-05 15:54:31 -0500

    Completed Search Stretch Goal

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,28 +2,29 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 
-function addToCollection( songName, artistName, year) {
-    let newSong = {
-        title: songName,
+function addToCollection( albumName, artistName, year) {
+    let newAlbum = {
+        title: albumName,
         artist: artistName,
         yearPublished: year
     }
-    collection.push(newSong);
+    collection.push(newAlbum);
     console.log(collection[collection.length - 1]);
 }
 
-addToCollection( 'Falling Down', 'Lil Peep', 2018);
-addToCollection( 'Kickstart My Heart', 'Motley Crue', 1989);
+addToCollection( 'Come Over When You\'re Sober, Part. 2', 'Lil Peep', 2018);
+addToCollection( 'Dr. Feelgood', 'Motley Crue', 1989);
 addToCollection( 'Star Shopping', 'Lil Peep', 2017);
-addToCollection( 'Title Track', 'MGK', 2020);
-addToCollection( 'Rapid Roy', 'Jim Croce', 1972);
+addToCollection( 'Tickets to My Downfall', 'MGK', 2020);
+addToCollection( 'Photographs & Memories', 'Jim Croce', 1972);
 addToCollection( 'Mama Tried', 'Merle Haggard & The Strangers', 1968);
 
 console.log(collection);
 
 function showCollection( collectionIndex ) {
-    console.log('The number ' + collectionIndex + " song in this collection is " + collection[collectionIndex].title);
-    console.log('There are ' + collection.length + ' songs in this collection');
+    console.log('When searching the object at index 1, we expect Dr. Feelgood by Motley Crue');
+    console.log(collection[collectionIndex]);
+    console.log('There are ' + collection.length + ' Albums in this collection');
 
     for ( let i = 0; i<collection.length; i++) {
         console.log( collection[i].title + ' by ' + collection[i].artist + ' published in ' + collection[i].yearPublished);
@@ -50,3 +51,27 @@ console.log('Searching Merle Haggard & The Strangers, We expect 1 match found');
 findByArtist( 'Merle Haggard & The Strangers' )
 console.log('Searching G-Eazy, We expect 0 matches found');
 findByArtist( 'G-Eazy' )
+
+function search( artistName, year ){
+    let searchResults = [];
+    if (arguments.length > 0){
+        for ( let i = 0; i<collection.length; i++ ){
+            if ( artistName === collection[i].artist && year === collection[i].yearPublished){
+                searchResults.push(collection[i]);
+            } else {
+
+            }
+        }
+        return searchResults;
+
+    } else {
+        for ( let i = 0; i<collection.length; i++ ){
+            searchResults.push(collection[i]);
+        }
+        return searchResults;
+    }
+}
+
+console.log('We are searching for Lil Peep and 2018', search( 'Lil Peep', 2018));
+console.log('We are searching for Yo Gotti and 2022', search( 'Yo Gotti', 2022));
+console.log('We are leaving search blank', search());
